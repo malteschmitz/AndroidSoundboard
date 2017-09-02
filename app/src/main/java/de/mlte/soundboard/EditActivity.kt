377af92a90b86c001640ba2b4e2cf581adca7f30
Malte Schmitz <m@mlte.de>
2017-09-02 18:43:37 +0200

    Display current file name

--- app/src/main/java/de/mlte/soundboard/EditActivity.kt
+++ app/src/main/java/de/mlte/soundboard/EditActivity.kt
@@ -7,8 +7,15 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.provider.OpenableColumns
+import android.widget.TextView
 
 class EditActivity : AppCompatActivity() {
+
+    private fun displayFile() {
+        val fileTextView = findViewById<TextView>(R.id.fileTextView)
+        currentUri?.let { uri -> fileTextView.setText(getFileName(uri)) }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +23,9 @@ class EditActivity : AppCompatActivity() {
 
         val captionEditText = findViewById<EditText>(R.id.captionEditText)
         captionEditText.setText(intent.getStringExtra("caption"))
+
+        currentUri = intent.getParcelableExtra<Uri>("uri")
+        displayFile();
 
         val okButton = findViewById<Button>(R.id.okButton)
         okButton.setOnClickListener {
@@ -43,6 +53,29 @@ class EditActivity : AppCompatActivity() {
 
         if (requestCode == 123 && resultCode == Activity.RESULT_OK && data != null) {
             currentUri = data.data
+            displayFile()
         }
+    }
+
+    fun getFileName(uri: Uri): String {
+        var result: String? = null
+        if (uri.scheme == "content") {
+            val cursor = contentResolver.query(uri, null, null, null, null)
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                }
+            } finally {
+                cursor!!.close()
+            }
+        }
+        if (result == null) {
+            result = uri.path
+            val cut = result!!.lastIndexOf('/')
+            if (cut != -1) {
+                result = result.substring(cut + 1)
+            }
+        }
+        return result
     }
 }
